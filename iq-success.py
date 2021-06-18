0beb15150879b5b255af10c43cfe2da816486903
Miguel Suarez <miguelsuarez>
2021-06-18 15:53:24 -0400

    last requested changes

--- iq-success.py
+++ iq-success.py
@@ -6,12 +6,13 @@ from operator import itemgetter
 # import urllib
 
 
-authorization = ""
+
 iq_url = "http://localhost:8070"
 output_json_file = "results_1.json"
-app_filter = "__bad"
+# TODO: this might be repeated
+app_filter = "z-"
 app_starts_filter = 'z-'
-org_filter = "__bad"
+org_filter = "z-"
 target_category = 'Distributed'
 top_number_of_components = 15
 top_number_of_apps = 15
@@ -23,7 +24,7 @@ iq_session = requests.Session()
 iq_session.auth = requests.auth.HTTPBasicAuth(cred[0], cred[1])
 iq_session.cookies.set('CLM-CSRF-TOKEN', 'api')
 iq_headers = {'X-CSRF-TOKEN': 'api', 'Content-Type': 'application/json', 'Accept': 'application/json'}
-orgs, apps, tags, report, target, csv, risk, application_risk, components, risky_components, mttr, org_mttr  = {}, {}, {}, {}, [], [], {}, {}, {}, {}, {}, {}
+orgs, apps, tags, report, target, csv, risk, application_risk, components, risky_components, mttr, org_mttr, org_names, direct_comp_build, direct_comp_release, org_list, apps_mttr  = {}, {}, {}, {}, [], [], {}, {}, {}, {}, {},{}, [], [], [], [], {}
 
 #------------------------------
 # last 3 month
@@ -93,7 +94,10 @@ def main():
         _row(str(key) + ', ' + str(top_apps[key]))
     #_row(str(res))
     _row(devide())
+    _row('MTTR')
+    _row(devide())
     #MTTR
+    orgs = get_orgs()
     mttr = get_metrics(date_range[0], date_range[-1])
     total_mttr = 0    
     for value in mttr:
@@ -103,25 +107,52 @@ def main():
         app_mttr = 0
         for aggregation in aggregations:
             if aggregation['mttrLowThreat'] !=None:
-                total_mttr += aggregation['mttrLowThreat']
+                #total_mttr += aggregation['mttrLowThreat']
                 app_mttr += aggregation['mttrLowThreat']
             if aggregation['mttrModerateThreat'] !=None:
-                total_mttr += aggregation['mttrModerateThreat']
+                # total_mttr += aggregation['mttrModerateThreat']
                 app_mttr += aggregation['mttrModerateThreat']
             if aggregation['mttrSevereThreat'] !=None:
-                total_mttr += aggregation['mttrSevereThreat']
+                # total_mttr += aggregation['mttrSevereThreat']
                 app_mttr += aggregation['mttrSevereThreat']
             if aggregation['mttrCriticalThreat'] !=None:
-                total_mttr += aggregation['mttrCriticalThreat']
+                # total_mttr += aggregation['mttrCriticalThreat']
                 app_mttr += aggregation['mttrCriticalThreat']
-            
-        if org not in org_mttr:
-            org_mttr[org] = app_mttr
-        else:
-            org_mttr[org] += app_mttr
-  
+        apps_mttr[application_name] = app_mttr
+    for k in list(apps_mttr.keys()):
+        if k.startswith(app_starts_filter):
+            del apps_mttr[k]
+    for key in apps_mttr:
+        days = (int) (apps_mttr[key]) / (1000*60*60*24)
+        _row(str(key) + ', ' + str(days) + ' days')
 
-
+        # if org not in org_mttr:
+            # org_mttr[application_name] = app_mttr
+        # else:
+        #     org_mttr[application_name] += app_mttr
+    # D vs T dependencies
+    direct_build = 0
+    direct_release = 0
+    for value in apps:
+        reports = get_reports(apps[value]['id'])
+        for report_ in reports:
+            if report_["stage"] == "build":
+                app_report_url = report_["reportDataUrl"]
+                raw_ = get_url("/"+app_report_url)
+                components_ = raw_["components"]
+                for component in components_:
+                    if "dependencyData" in component and component["dependencyData"]["directDependency"]:
+                        direct_build+=1
+                        direct_comp_build.append(component["displayName"]) 
+            if report_["stage"] == "release":
+                app_report_url = report_["reportDataUrl"]
+                raw_ = get_url("/"+app_report_url)
+                components_ = raw_["components"]
+                for component in components_:
+                    if "dependencyData" in component and component["dependencyData"]["directDependency"]:
+                        direct_release+=1
+                        direct_comp_release.append(component["displayName"]) 
+    _row(devide())
     _row('All applications')
     _row(devide())
     _row(header)
@@ -134,8 +165,11 @@ def main():
     _row(['open medium']+rep_data("all",_open,high))
     root_risk = total_risk/len(apps)
     _row('average risk,'+str(root_risk))
-    root_days = (int) ((total_mttr/(range_*4)) / (1000*60*60*24))
-    _row('root mttr,' + str(root_days))
+    #root_days = (int) ((total_mttr/(range_*4)) / (1000*60*60*24))
+    #_row('root mttr,' + str(root_days))
+    _row(devide())
+    _row('root direct dependencies - build,' + str(direct_build))
+    _row('root direct dependencies - release,' + str(direct_release))
     _row(devide())
 
     for org in orgs.values():
@@ -146,10 +180,10 @@ def main():
         _row(['open critical']+rep_data(name,_open,crit))
         _row(['open high']+rep_data(name,_open,high))
         _row(['open medium']+rep_data(name,_open,med))
-        org_risk = risk[org['name']]/len(org['apps'])
-        _row('average risk,'+str(org_risk))
-        days = (int) ((org_mttr[org['name']]/(range_*4))/ (1000*60*60*24))
-        _row('org mttr,' + str(days))
+        # org_risk = risk[org['name']]/len(org['apps'])
+        # _row('average risk,'+str(org_risk))
+        # days = (int) ((org_mttr[org['name']]/(range_*4))/ (1000*60*60*24))
+        # _row('org mttr,' + str(days))
         _row(devide())
 
     _row(target_category)
@@ -265,11 +299,14 @@ def set_orgs():
     url = '/api/v2/organizations'
     resp = get_url(url)["organizations"]
     for o in resp:
-        if not o['id'] == 'ROOT_ORGANIZATION_ID':
+        # if not o['id'] == 'ROOT_ORGANIZATION_ID':
+        if not o['id'] == 'ROOT_ORGANIZATION_ID' and not o['name'].startswith(app_starts_filter):
             orgs.update({
              o["id"] : {'name': o["name"],
              'apps':[], 'target':[]}
             })
+            org_list.append(o['id'])
+ 
 
 def add_org(orgId, publicId):
     orgs[orgId]['apps'].append(publicId)
@@ -376,17 +413,17 @@ def set_applications():
     resp = [aa for aa in resp if not prune_app(aa)]
 
     for app in resp:
-        _id, publicId = app['id'], app["publicId"]
+        if app['organizationId'] in org_list:
+            _id, publicId = app['id'], app["publicId"]
+            add_org( app['organizationId'] , publicId)
+            add_target( app['applicationTags'] , app['organizationId'], publicId)
+            app = {
+            'id': _id,
+            'history': get_app_history(_id),
+            'metrics': get_app_metrics(_id)
 
-        add_org( app['organizationId'] , publicId)
-        add_target( app['applicationTags'] , app['organizationId'], publicId)
-        app = {
-         'id': _id,
-         'history': get_app_history(_id),
-         'metrics': get_app_metrics(_id)
-
-        }
-        apps.update({ publicId : app })
+            }
+            apps.update({ publicId : app })
 
 
 def prune_app(app, prune = False):
@@ -395,13 +432,14 @@ def prune_app(app, prune = False):
         prune = app['name'].startswith(app_filter)
 
     # prune if org name starts with filter
-    if "org_filter" in globals() and not prune:
-        org_name = get_org_name(app['organizationId'])
-        prune = org_name.startswith(org_filter)
+    # if "org_filter" in globals() and not prune:
+    #     org_name = get_org_name(app['organizationId'])
+    #     prune = org_name.startswith(org_filter)
 
     return prune
 #-------------------
-
+def get_app_reports(appId):
+    resp = get_reports(appId)
 
 def get_app_metrics(appId):
     resp = get_metrics( date_range[0], date_range[-1], [appId])
@@ -491,6 +529,11 @@ def get_metrics(start, end, appId=[], orgId=[] ):
      "applicationIds": appId, "organizationIds": orgId
     }
     resp = post_url(url, payload)
+    return resp
+
+def get_reports(appId):
+    url = f'/api/v2/reports/applications/{appId}'
+    resp = get_url(url)
     return resp
 
 def last_scanned(history):
