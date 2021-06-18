26cc8d7a715fea1aac189d5139fefeeb8eb7eebd
Miguel Suarez <miguelsuarez>
2021-06-18 15:53:48 -0400

    last requested changes

--- iq-success.py
+++ iq-success.py
@@ -6,7 +6,7 @@ from operator import itemgetter
 # import urllib
 
 
-
+authorization = "user:pass"
 iq_url = "http://localhost:8070"
 output_json_file = "results_1.json"
 # TODO: this might be repeated
